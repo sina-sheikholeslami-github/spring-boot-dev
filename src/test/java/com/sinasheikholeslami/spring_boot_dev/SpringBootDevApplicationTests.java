f7d476540d47468fd8f460caf8410c942ae1a9cb
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-07-23 11:52:32 -0700

    initial commit

---
+++ src/test/java/com/sinasheikholeslami/spring_boot_dev/SpringBootDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.spring_boot_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
